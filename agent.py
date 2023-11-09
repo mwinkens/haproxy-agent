762b791da9c31fb1f30ec2a0e87d3ac4dca6f101
Marvin Winkens <m.winkens@fz-juelich.de>
2023-11-09 12:25:56 +0100

    add agent and a testfile for the nagios ram check

---
+++ agent.py
@@ -0,0 +1,89 @@
+import importlib
+import socketserver
+import argparse
+import sys
+from pathlib import Path
+import importlib.util
+
+ram_check_module_name = "check_ram"
+
+
+def import_ramcheck_module(file_path: Path):
+    """
+    Imports the module at filepath and adds it to the known system modules
+    :param file_path:
+    :return:
+    """
+    spec = importlib.util.spec_from_file_location(ram_check_module_name, file_path.absolute())
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    sys.modules[ram_check_module_name] = module
+
+
+class TCPHaproxyHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+        # get ram state
+        check_ram = importlib.import_module(ram_check_module_name)
+        message, state = check_ram.check_ram(warning_threshold=20, critical_threshold=10, percent=True, verbosity=False,
+                                             nocache=False)
+        # TODO maybe we also want to check the state, maybe not
+
+        # message parsing
+        text_chunks = message.split()
+        ram_percentage = text_chunks[2][:-1]  # third chunk, remove '%' text
+
+        # weight is how good the service currently is, everything up to 70% ram usage is still valid
+        # ram warnings usually start at 80%
+        weight = 100
+        degrading_treshhold = 30  # start degrading service at last 30% ram
+        fully_degraded_treshold = 5  # set weight to 0 at threshold
+
+        if ram_percentage < fully_degraded_treshold:
+            weight = 0
+        elif ram_percentage < degrading_treshhold:
+            # linear descent, 15% ram left would make weight to 50%
+            weight = (weight * ram_percentage) // degrading_treshhold
+
+        self.request.sendall(f"{weight}%")
+
+
+def main(host, port, check_ram_path):
+    # check that the nagios ram check exists
+    ram_check = Path(check_ram_path)
+    if not ram_check.is_file():
+        print(f"{ram_check.absolute()} does not exist or is not a file!")
+        sys.exit(1)
+
+    # import the ram check module from nagios
+    import_ramcheck_module(ram_check)
+
+    # Create the server, binding to localhost on the port
+    print(host, port)
+    with socketserver.TCPServer((host, port), TCPHaproxyHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='health-agent',
+                                     description='health check agent for haproxy using nagios ramcheck',
+                                     epilog="I can't believe somebody is reading this")
+    parser.add_argument('-host', '--hostname', required=False, default="127.0.0.1")
+    parser.add_argument('-p', '--port', type=int, required=False, default=3000)
+    parser.add_argument('nagios_path')
+    args = parser.parse_args()
+    main(args.hostname, args.port, args.nagios_path)
