6b80429c18e15ba59303b1f1717f3b18fc8506d3
Marvin Winkens <m.winkens@fz-juelich.de>
2023-11-09 16:00:25 +0100

    add crude nagios examples which don't check the ram but return warning or critical with certain percentage values

---
+++ nagios-examples/test_ram_check_85.py
@@ -0,0 +1,9 @@
+OK = 0
+WARNING = 1
+CRITICAL = 2
+UNKNOWN = 3
+
+def check_ram(warning_threshold, critical_threshold, percent, verbosity, nocache):
+    MESSAGE = "RAM WARNING: %d%% ram free (%d/%d MB used)" % (100-85, 100, 100)
+    STATE = CRITICAL
+    return [MESSAGE, STATE]
